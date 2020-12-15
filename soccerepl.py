78c96d190621232466f173ff34ed96bbd45a8297
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-12-16 04:42:40 +0600

    Update soccerepl.py

--- soccerepl.py
+++ soccerepl.py
@@ -5,9 +5,9 @@ import os
 
 soccer_data = SoccerDataAPI()
 
-if os.path.exists("epl.txt"):
-    os.remove("epl.txt")
-f = open("epl.txt", "a")
+if os.path.exists("txt_epl.txt"):
+    os.remove("txt_epl.txt")
+f = open("txt_epl.txt", "a")
 epl = soccer_data.english_premier()
 for et in epl:
     str1 = et['team']+","+ et['pos']+","+et['points']+","+et['matches_played']+","+et['wins']+","+et['losses']+","+et['goals_for']+","+et['goal_diff']+"\n"
